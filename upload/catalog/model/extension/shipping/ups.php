8125a13e7478e30a463fd099a1c7e42aea847e89
Ernie <ernst@jacob.ch>
2021-03-23 23:25:27 +0000

    Update ups.php
    Updated UPS Link

--- upload/catalog/model/extension/shipping/ups.php
+++ upload/catalog/model/extension/shipping/ups.php
@@ -203,7 +203,7 @@ class ModelExtensionShippingUps extends Model {
 			$xml .= '</RatingServiceSelectionRequest>';
 
 			if (!$this->config->get('ups_test')) {
-				$url = 'https://www.ups.com/ups.app/xml/Rate';
+				$url = 'https://onlinetools.ups.com/ups.app/xml/Rate';
 			} else {
 				$url = 'https://wwwcie.ups.com/ups.app/xml/Rate';
 			}
